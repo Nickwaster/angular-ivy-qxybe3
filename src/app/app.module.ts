e5f45e6bdf3b70b69d4b3392f103f2d8cbbf7b7f
Nickwaster <pattenicolas20@gmail.com>
2021-01-14 12:40:20 +0100

    1 file modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-
-import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
-import { FicheProduitComponent } from './fiche-produit/fiche-produit.component';
-import { SuiteFiboComponent } from './suite-fibo/suite-fibo.component';
-import { UsersListComponent } from './users-list/users-list.component';
-
+import { AppComponent } from "./app.component";
+import { MenuComponent } from "./menu/menu.component";
+import { FicheProduitComponent } from "./fiche-produit/fiche-produit.component";
+import { SuiteFiboComponent } from "./suite-fibo/suite-fibo.component";
+import { UsersListComponent } from "./users-list/users-list.component";
+import { UserCardComponent } from "./user-card/user-card.component";
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     FicheProduitComponent,
     SuiteFiboComponent,
-    UsersListComponent
+    UsersListComponent,
+    UserCardComponent
   ],
-  imports: [
-
-    BrowserModule,
-  ],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
