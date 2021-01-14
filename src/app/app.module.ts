43cb52cd603281bb1da6c4ab3677e446e8148a33
Nickwaster <pattenicolas20@gmail.com>
2021-01-14 12:34:39 +0100

    2 files modified, 3 added, 2 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { FicheProduitComponent } from './fiche-produit/fiche-produit.component';
@@ -17,10 +17,8 @@ import { UsersListComponent } from './users-list/users-list.component';
     UsersListComponent
   ],
   imports: [
-,
-    Br,
-wserModule,
-    AppRoutingModule
+
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
