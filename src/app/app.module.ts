c60fc7e784e87c18ead884ef34c29fd98d2e33eb
Nickwaster <pattenicolas20@gmail.com>
2021-01-14 12:11:28 +0100

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { FicheProduitComponent } from './fiche-produit/fiche-produit.component';
 import { MenuComponent } from './menu/menu.component';
+import { FicheProduitComponent } from './fiche-produit/fiche-produit.component';
 import { SuiteFiboComponent } from './suite-fibo/suite-fibo.component';
-import { UserCardComponent } from './user-card/user-card.component';
-import { UserListComponent } from './user-list/user-list.component';
+import { UsersListComponent } from './users-list/users-list.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, FicheProduitComponent, MenuComponent, SuiteFiboComponent, UserCardComponent, UserListComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    FicheProduitComponent,
+    SuiteFiboComponent,
+    UsersListComponent
+  ],
+  imports: [
+,
+    Br,
+wserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
